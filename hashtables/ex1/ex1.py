275c22b955561ed32ad765f0b956f9b1b1050bf1
danpatrickoneill <danpatrickoneill@gmail.com>
2020-06-04 11:37:18 -0400

    Tests passing; logic could potentially be tighter

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    weights_table = {}
+    for i, weight in enumerate(weights):
+        weights_table[weight] = (limit - weight, i)
+        if i == 0:
+            continue
+        if limit - weight in weights_table:
+            if weight == limit - weight:
+                return (weights_table[weight][1], weights.index(weight))
+            return (weights_table[weight][1], weights_table[limit-weight][1])
+        
 
     return None
+
+weights_2 = [4, 4]
+answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+
+print(answer_2)
